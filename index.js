8f4a4856d798e2ed9c57ab0f51fdc6976075eb58
Stuart <stu.feldt@gmail.com>
2014-06-17 20:44:00 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,206 @@
+
+$('#county').change(function(){
+  var county = $(this).find(":selected").attr("value");
+  populate_graph("county", county, 1000);
+});
+
+var w = 950;
+var h = 500;
+var dataset;
+
+var svg = d3.select("#graph-container")
+  .append("svg")
+  .append("g")
+  .attr("width", w)
+  .attr("height", h)
+  .attr("id", "graph");
+
+var yaxis = svg.append("g")
+        .attr("class", "yaxis")
+        .attr("transform", "translate(0,0)");
+
+var xaxis = svg.append("g")
+  .attr("class", "xaxis")
+  .attr("transform", "translate(0,0)");
+
+xaxis.append("line")
+    .attr("x1", 0)
+    .attr("y1", h)
+    .attr("x2", w+100)
+    .attr("y2", h)
+    .attr("stroke", "#222")
+    .attr("stroke-width", "1");
+
+for(var y = 0; y <15; y++) {
+  var year = 2000 + y;
+  xaxis.append("text")
+    .text(year)
+    .attr("x", (w/14)*y)
+    .attr("y", h + 20);
+}
+/*var info = d3.select("body").append("div")
+                   .attr("id", "info");
+*/
+function update(data, speed, max) {      
+        yaxis.selectAll('*').remove();
+        var yaxis_mks = [];
+        for(var i = 0; i<=max; i+=(Math.round(max/10))+1) {
+          /*var mrk = yaxis.append("line")
+          .attr("y2", h+30)
+          .attr("y1", h+30)
+          .transition()
+          .duration(speed)
+          .attr("x1", 15)
+          .attr("x2", 20)
+          .attr("y2", calculate_y_pos_injured(i, max))
+          .attr("y1", calculate_y_pos_injured(i, max))
+          .attr("stroke", "#222")
+          .attr("stroke-width", "1");*/
+
+        var mrk_txt = yaxis.append("text")
+          .attr("y", h)
+          .transition()
+          .duration(speed)
+          .attr("x", 0)
+          .attr("y", calculate_y_pos_injured(i, max)+4)
+          .text(i);
+
+          yaxis_mks.push({mk_txt: mrk_txt})
+        }
+
+        var firstdate= data[0].time;
+        // DATA JOIN
+        // Join new data with old elements, if any.
+        var circles = svg.selectAll("circle")
+            .data(data, function(d) { return d.id; });
+        // ENTER
+      // Create new elements as needed.
+      circles.enter()
+            .append("circle")
+            .attr("cy", h)
+            .attr("r", function(d) {
+                return Math.log(d.damage+1)+3;
+              })
+              .attr("fill", function(d) {
+                if(d.type == 'Tornado') {
+                    return "#940";
+                } else if(d.type == "Winter Storm" || d.type == "Heavy Snow") {
+                    return "#049";
+                } else if(d.type == "Hail") {
+                    return "#88a";
+                } else if(d.type == "Thunderstorm Wind") {
+                    return "#484";
+                } else if(d.type = "Heat" || d.type == "Excessive Heat") {
+                  return "#c03";
+                }
+              })
+              /*.on("mouseover", function(d){
+                info.html("<h3>" + d.mag + " " + d.type + "</h3>" + d.location + ", " + d.county + "<p>" + d.date + "</p><p>" + d.injuries + " injured</p><p>" + d.deaths + " deaths</p>$" + d.damage + " in damages.");
+              })*/
+            .transition()
+              .duration(speed)
+              .style("fill-opacity", .65)
+              .attr("cx", function(d, i) {
+                return ((d.time - firstdate) / 475000) + 25;
+              })
+              .attr("cy", function(d){
+                return calculate_y_pos_injured((parseInt(d.injuries) + parseInt(d.deaths)), max);
+              })
+              ;
+
+           circles
+           .tooltip(function(d, i) {
+                var r, svg2;
+                r = +d3.select(this).attr('r');
+                svg2 = d3.select(document.createElement("svg")).attr("height", 50);
+                g = svg2.append("g");
+                g.append("rect").attr("width", r * 10).attr("height", 10);
+                g.append("text").text("10 times the radius of the cirlce").attr("dy", "25");
+                return {
+                  type: "tooltip",
+                  text: "<h3>"+d.mag+" "+d.type+"</h3><p><span class='bottom-tip'>"+d.deaths+" dead</span>|<span class='bottom-tip'>"+d.injuries+" injured</span></p><p>"+d.date+"</p><p>"+d.location+", "+d.county+"</p><p>$"+numberWithCommas(d.damage)+" in damage</p></div>",
+                  detection: "shape",
+                  gravity: "right",
+                  placement: "fixed",
+                  position: [((d.time - firstdate) / 475000) + 25, calculate_y_pos_injured((parseInt(d.injuries) + parseInt(d.deaths)), max)],
+                  displacement: [r, -68],
+                  mousemove: false
+                };
+              })
+           .transition()
+              .duration(speed)
+              .style("fill-opacity", .65)
+              .attr("cx", function(d, i) {
+                return ((d.time - firstdate) / 475000) + 25;
+              })
+              .attr("cy", function(d){
+                return calculate_y_pos_injured((parseInt(d.injuries) + parseInt(d.deaths)), max);
+              });
+              
+           
+
+
+          circles.exit().transition()
+              .duration(speed)
+              .attr("cy", h)
+              .style("fill-opacity", 1e-6)
+              .remove();
+  
+}
+
+function calculate_y_pos_injured(num, max){
+  var m = (h - 35)/max;
+  return h-(num*m) - 30;
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function populate_graph(q, p, s) {
+    
+    switch(p) {
+        case "tornado":
+            $('a').removeClass('active');
+            $('#tornado').addClass('active');
+            $('#county').val(false);
+            break;
+        case "hail":
+            $('a').removeClass('active');
+            $('#hail').addClass('active');
+            $('#county').val(false);
+            break;
+        case "heat":
+            $('a').removeClass('active');
+            $('#heat').addClass('active');
+            $('#county').val(false);
+            break;
+        case "winter storm":
+            $('a').removeClass('active');
+            $('#winter').addClass('active');
+            $('#county').val(false);
+            break;
+        case "thunderstorm wind":
+            $('a').removeClass('active');
+            $('#wind').addClass('active');
+            $('#county').val(false);
+            break;
+        default:
+            $('a').removeClass('active');
+            $('#all').addClass('active');
+
+    }
+
+    var url = "ws/getData.php";
+
+    if(q != false) {
+        url += "?q=" + q + "&p=" + p;
+    }
+
+    $.getJSON(url, function(data){
+        dataset = data;
+          update(data.data, s, data.max);
+        });
+}
+
+populate_graph(false, false, 2500);            
---
+++ ws/getData.php
@@ -0,0 +1,96 @@
+<?php
+include("database.php");
+
+$con=mysqli_connect($database_host,$database_user,$database_password,$database_schema);
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$j = array();
+$high_injury = 0;
+
+if(isset($_GET['q']) && isset($_GET['p'])) {
+	$q = $_GET['q'];
+	$p = $_GET['p'];
+
+	$query = sprintf("SELECT * FROM data WHERE %s='%s' order by date asc",
+            mysql_real_escape_string($q),
+            mysql_real_escape_string($p));
+
+	$result = mysqli_query($con,$query);
+
+	
+	while($row = mysqli_fetch_array($result)) {
+
+		$d = "";
+		if(strpos($row['property_damage'], 'K')) {
+			$d = substr($row['property_damage'],0,strpos($row['property_damage'], 'K'));
+			$d = substr($d,0,strpos($d,"."));
+			$d .= "000";
+			$d = intval($d);
+		} else {
+			$d = substr($row['property_damage'],0,strpos($row['property_damage'], 'M'));
+			$d = substr($d,0,strpos($d,"."));
+			$d .= "000000";
+			$d = intval($d);
+		}
+		if(intval($row['deaths']) + intval($row['injuries']) > $high_injury) {
+			$high_injury = intval($row['deaths']) + intval($row['injuries']);
+		}
+	  array_push($j, array("location" => $row['location'], 
+	  	"county" => $row['county'],
+	  	"time" => strtotime($row['date']),
+	  	"date" => $row['date'],
+	  	"type" => $row['type'],
+	  	"mag" => $row['mag'],
+	  	"hail_size" => split(" in.", $row['mag'])[0],
+	  	"deaths" => $row['deaths'],
+	  	"injuries" => $row['injuries'],
+	  	"damage" => $d,
+	  	'id' => $row['iddata'],
+	  	"state" => $row['state'],));
+	}
+
+
+} else {
+	$query = "SELECT * FROM data where injuries > 0 or deaths > 0 or property_damage != '0.00K' order by date asc";
+
+	$result = mysqli_query($con,$query);
+
+	while($row = mysqli_fetch_array($result)) {
+
+		$d = "";
+		if(strpos($row['property_damage'], 'K')) {
+			$d = substr($row['property_damage'],0,strpos($row['property_damage'], 'K'));
+			$d = substr($d,0,strpos($d,"."));
+			$d .= "000";
+			$d = intval($d);
+		} else {
+			$d = substr($row['property_damage'],0,strpos($row['property_damage'], 'M'));
+			$d = substr($d,0,strpos($d,"."));
+			$d .= "000000";
+			$d = intval($d);
+		}
+		if(intval($row['deaths']) + intval($row['injuries']) > $high_injury) {
+			$high_injury = intval($row['deaths']) + intval($row['injuries']);
+		}
+
+	  array_push($j, array("location" => $row['location'], 
+	  	"county" => $row['county'],
+	  	"time" => strtotime($row['date']),
+	  	"date" => $row['date'],
+	  	"type" => $row['type'],
+	  	"mag" => $row['mag'],
+	  	"hail_size" => split(" in.", $row['mag'])[0],
+	  	"deaths" => $row['deaths'],
+	  	"injuries" => $row['injuries'],
+	  	"damage" => $d,
+	  	'id' => $row['iddata'],
+	  	"state" => $row['state'],));
+	}
+}
+
+echo json_encode(array("data" => $j, "max" => $high_injury));
+
+?>
